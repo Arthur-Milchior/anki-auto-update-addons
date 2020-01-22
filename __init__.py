235fb0f80e9e90583f4fc22d6a931677382abbd9
Arthur Milchior <Arthur@Milchior.fr>
2020-01-22 05:32:06 -0800

    update for 2.1.20

--- __init__.py
+++ __init__.py
@@ -5,7 +5,12 @@ old_sync = AnkiQt._sync
 def _sync(self):
     old_sync(self)
     ad =AddonsDialog(self.addonManager)
-    ad.onCheckForUpdates()
+    if hasattr(ad, "onCheckForUpdates"):
+        #version at most 19
+        ad.onCheckForUpdates()
+    else:
+        #version at least 20
+        ad.check_for_updates()
     ad.reject()
     
 AnkiQt._sync = _sync
